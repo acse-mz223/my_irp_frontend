131463bc9cb85b52e6adcabb18bd126fb2d1810c
acse-mz223 <146830449+acse-mz223@users.noreply.github.com>
2024-08-27 00:32:06 +0100

    Delete src directory

--- src/utility.js
+++
@@ -1,9 +0,0 @@
-// 字符串转小数-》保留三位小数的字符串 -》字符串转化为小数
-export function formatNumber(value) {
-  if (typeof(value) === "number")
-    return parseFloat(value).toPrecision(3);
-  else 
-  return parseFloat(Number(value)).toPrecision(3);
-  }
-
-
